c7f9026b5d537331dbe6ce3e28c9dcb77dba81e6
EternalZSY <910221550@qq.com>
2025-08-20 21:07:12 +0800

    init

---
+++ js/qdb.js
@@ -0,0 +1,146 @@
+
+class QDB {
+
+    #DB_NAME = "question_db";
+    #DB_VERSION = 1;
+    #STORE_QUESTION = "questions";
+    #STORE_STATE = "states";
+    #STORE_STAR = "stars";
+
+    #db = null;
+
+    deconstructor() {
+        this.close();
+    }
+
+    open() {
+        let request = indexedDB.open(this.#DB_NAME, this.#DB_VERSION);
+
+        request.onupgradeneeded = (e) => {
+            console.debug('数据库初始化');
+            this.#db = e.target.result;
+            this.#initDB(this.#db);
+        }
+
+        return new Promise((resolve, reject) => {
+            request.onsuccess = (e) => {
+                console.debug('数据库打开完成');
+                this.#db = e.target.result;
+                resolve(this.#db);
+            }
+            request.onerror = (e) => {
+                console.error('数据库打开错误', e.target.error);
+                reject(e.target.error);
+            }
+        })
+    }
+
+    close() {
+        if (this.#db) {
+            this.#db.close();
+            this.#db = null;
+        }
+    }
+
+    #initDB(db) {
+        if (!db.objectStoreNames.contains(this.#STORE_QUESTION)) {
+            const store = db.createObjectStore(this.#STORE_QUESTION, {
+                keyPath: "_idx",
+                autoIncrement: true
+            });
+            store.createIndex("by_id", "id", { unique: true });
+        }
+        if (!db.objectStoreNames.contains(this.#STORE_STATE)) {
+            const st = db.createObjectStore(this.#STORE_STATE, { keyPath: "id" });
+        }
+        if (!db.objectStoreNames.contains(this.#STORE_STAR)) {
+            const st = db.createObjectStore(this.#STORE_STAR, { keyPath: "id" });
+        }
+    }
+
+    #toPromise(request) {
+        return new Promise((resolve, reject) => {
+            request.onsuccess = (e) => {
+                resolve(e.target.result);
+            }
+            request.onerror = (e) => {
+                reject(e.target.error);
+            }
+        })
+    }
+
+    #createTransactionStore(name) {
+        return this.#db.transaction(name, "readwrite").objectStore(name);
+    }
+
+    ///////////////////////////
+
+    readQuestion(id) {
+        let request = this.#createTransactionStore(this.#STORE_QUESTION).index("by_id").get(id);
+        return this.#toPromise(request);
+    }
+
+    writeQuestion(q) {
+        let request = this.#createTransactionStore(this.#STORE_QUESTION).put(q);
+        return this.#toPromise(request);
+    }
+
+    readAllQuestions() {
+        let request = this.#createTransactionStore(this.#STORE_QUESTION).getAll();
+        return this.#toPromise(request);
+    }
+
+    clearQuestions() {
+        let request = this.#createTransactionStore(this.#STORE_QUESTION).clear();
+        return this.#toPromise(request);
+    }
+
+    ////////////////
+
+    getState(id) {
+        let request = this.#createTransactionStore(this.#STORE_STATE).get(id);
+        return this.#toPromise(request);
+    }
+    getAllStates() {
+        let request = this.#createTransactionStore(this.#STORE_STATE).getAll();
+        return this.#toPromise(request);
+    }
+
+    setState(id, state) {
+        let request = this.#createTransactionStore(this.#STORE_STATE).put({ id: id, state: state });
+        return this.#toPromise(request);
+    }
+
+    clearStates() {
+        let request = this.#createTransactionStore(this.#STORE_STATE).clear();
+        return this.#toPromise(request);
+    }
+
+    /////////////////////
+
+    setStar(id, star) {
+        let request = null;
+        if (star) {
+            request = this.#createTransactionStore(this.#STORE_STAR).put({ id: id });
+        } else {
+            request = this.#createTransactionStore(this.#STORE_STAR).delete(id);
+        }
+        return this.#toPromise(request);
+    }
+
+    getStar(id) {
+        let request = this.#createTransactionStore(this.#STORE_STAR).get(id);
+        return this.#toPromise(request);
+    }
+
+    getAllStars() {
+        let request = this.#createTransactionStore(this.#STORE_STAR).getAll();
+        return this.#toPromise(request);
+    }
+
+    clearStars() {
+        let request = this.#createTransactionStore(this.#STORE_STAR).clear();
+        return this.#toPromise(request);
+    }
+
+}
---
+++ js/qmgr.js
@@ -0,0 +1,117 @@
+
+class QuestionsManager {
+
+    #db = null;
+    currentQuestions = [];
+    currentLevel = "a";
+    onlyDiff = false;
+
+    async init(servVer) {
+
+        // 打开数据库
+        this.#db = new QDB();
+        await this.#db.open();
+
+        // 按需更新题库
+        if (servVer && this.needUpdate(servVer)) {
+            await this.updateQuestions();
+            localStorage.setItem("q_ver", servVer);
+        }
+
+        // 根据等级获取题表
+        this.currentLevel = localStorage.getItem("q_level") || "a";
+        this.onlyDiff = localStorage.getItem("q_onlyDiff") === "true";
+        this.currentQuestions = await this.getLevelQuestions(this.currentLevel, this.onlyDiff);
+        console.debug('获取当前题表', this.currentLevel, this.currentQuestions.length);
+
+    }
+
+    needUpdate(servVer) {
+        const localVer = localStorage.getItem("q_ver");
+        return localVer == null || localVer !== servVer;
+    }
+
+    async updateQuestions() {
+
+        //下载新题库并从json转换为js对象
+        let resp = await fetch("data/questions.json");
+        let qs = await resp.json();
+        console.debug('新题库下载完成');
+
+        //清空原题库
+        await this.#db.clearQuestions();
+
+
+        //添加新题库
+        let result = [];
+        for (const q of qs) {
+            await this.#db.writeQuestion(q);
+            result.push(q);
+        }
+
+        console.debug('解析题库,写入数据库完成');
+
+        //返回题库列表
+        return result;
+    }
+
+    async getLevelQuestions(level, onlyDiff) {
+
+        let qs = await this.#db.readAllQuestions();
+        let ss = await this.#db.getAllStates();
+        let stars = await this.#db.getAllStars();
+        if (level !== 'all') {
+            qs = qs.filter(q => q[`in_${level}`]);
+        }
+
+        if (onlyDiff && level !== 'all' && level !== 'a') {
+            if (level === 'b') {
+                qs = qs.filter(q => q.in_b && !q.in_a);
+            } else if (level === 'c') {
+                qs = qs.filter(q => q.in_c && !q.in_b && !q.in_a);
+            }
+        }
+
+        for (let i = 0; i < qs.length; i++) {
+            qs[i].index = i;
+            qs[i].state = ss.find(ss => ss.id === qs[i].id)?.state || 0;
+            qs[i].star = !!stars.find(s => s.id === qs[i].id);
+        }
+
+        return qs;
+    }
+
+    async setState(id, state) {
+        const q = this.currentQuestions.find(q => q.id === id);
+        if (q) {
+            await this.#db.setState(id, state);
+            q.state = state;
+        }
+    }
+
+    async setStar(id, star) {
+        const q = this.currentQuestions.find(q => q.id === id);
+        if (q) {
+            await this.#db.setStar(id, star);
+            q.star = star;
+        }
+    }
+
+    async switchLevel(level, onlyDiff) {
+        this.currentQuestions = await this.getLevelQuestions(level, onlyDiff);
+        this.currentLevel = level;
+        this.onlyDiff = onlyDiff;
+        localStorage.setItem('q_level', level);
+        localStorage.setItem('q_onlyDiff', onlyDiff);
+    }
+
+    async resetProgress() {
+        await this.#db.clearStates();
+        this.currentQuestions.forEach(q => { q.state = 0; });
+        localStorage.removeItem('q_last_id');
+        localStorage.removeItem('q_last_study_id');
+    }
+
+    get Questions() { return this.currentQuestions; }
+}
+
